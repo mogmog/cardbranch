6bdad1d970f645a92d26be6e0a4577a50380262f
Graham <mogmog@gmail.com>
2018-04-11 17:39:31 +0100

    updated sql

--- api/sql/thing_public_pagecard.sql
+++ api/sql/thing_public_pagecard.sql
@@ -1,7 +1,8 @@
-INSERT INTO pagecard (id, "pageId", component) VALUES (5, 1, 'DistrictTimeCard');
-INSERT INTO pagecard (id, "pageId", component) VALUES (6, 1, 'DistrictInfoCard');
-INSERT INTO pagecard (id, "pageId", component) VALUES (7, 1, 'DistrictMostSimilarCard');
-INSERT INTO pagecard (id, "pageId", component) VALUES (2, 1, 'StoreGenderPercentCard');
-INSERT INTO pagecard (id, "pageId", component) VALUES (1, 1, 'StoreStreetViewCard');
-INSERT INTO pagecard (id, "pageId", component) VALUES (3, 1, 'StorePopularPostcodesCard');
-INSERT INTO pagecard (id, "pageId", component) VALUES (4, 2, 'StoreStreetViewCard');
+INSERT INTO public.pagecard (id, "pageId", component, enabled) VALUES (8, 2, 'StorePopularPostcodesCard', 'Y');
+INSERT INTO public.pagecard (id, "pageId", component, enabled) VALUES (1, 1, 'StoreStreetViewCard', 'Y');
+INSERT INTO public.pagecard (id, "pageId", component, enabled) VALUES (2, 1, 'StoreGenderPercentCard', 'Y');
+INSERT INTO public.pagecard (id, "pageId", component, enabled) VALUES (5, 1, 'DistrictTimeCard', 'Y');
+INSERT INTO public.pagecard (id, "pageId", component, enabled) VALUES (6, 1, 'DistrictInfoCard', 'Y');
+INSERT INTO public.pagecard (id, "pageId", component, enabled) VALUES (7, 1, 'DistrictMostSimilarCard', 'Y');
+INSERT INTO public.pagecard (id, "pageId", component, enabled) VALUES (3, 1, 'StorePopularPostcodesCard', 'N');
+INSERT INTO public.pagecard (id, "pageId", component, enabled) VALUES (4, 2, 'StoreStreetViewCard', 'Y');
